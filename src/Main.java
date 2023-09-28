f592366c2449ae35f342c45d4aecf6a34553a989
ballcaden <ballcaden@gmail.com>
2023-09-28 09:19:39 -0400

    init

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Objects;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String partyAffiliation;
+
+        System.out.println("Please enter your party affiliation(R, D, or I): ");
+        partyAffiliation = scan.nextLine();
+
+        if (Objects.equals(partyAffiliation, "D")) {
+            System.out.println("You get a democratic monkey!");
+        } else if (Objects.equals(partyAffiliation, "R")) {
+            System.out.println("You get a republican elephant!");
+        } else if (Objects.equals(partyAffiliation, "I")) {
+            System.out.println("You get an independent person");
+        }
+
+    }
+}
